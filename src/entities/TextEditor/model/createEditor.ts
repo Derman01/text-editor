5559f5c07b7e55ad57dce9393be0770d42f951d5
Denis <lettenderman@gmail.com>
2024-02-11 02:06:40 +0500

    1) add history 2) update gitignore

--- src/entities/TextEditor/model/createEditor.ts
+++ src/entities/TextEditor/model/createEditor.ts
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import { createEditor } from 'slate';
 import { withReact } from 'slate-react';
+import { withHistory } from 'slate-history';
 
 /**
  * Метод создания модели редактора
  */
-export const useEditor = () => withReact(createEditor());
+export const useEditor = () => withReact(withHistory(createEditor()));
